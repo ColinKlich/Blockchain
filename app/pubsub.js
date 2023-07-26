e80893f017465819466bd27559f3ae353e755a1a
ColinKlich <115095065+ColinKlich@users.noreply.github.com>
2023-07-26 13:33:03 -0500

    commit

--- app/pubsub.js
+++ app/pubsub.js
@@ -7,70 +7,76 @@ const credentials = {
 };
 
 const CHANNELS = {
-    TEST: 'TEST',
-    BLOCKCHAIN: 'BLOCKCHAIN',
-    TRANSACTION: 'TRANSACTION'
-  };
-  
-  class PubSub {
-    constructor({ blockchain, transactionPool }) {
-      this.blockchain = blockchain;
-      this.transactionPool = transactionPool;
-  
-      this.pubnub = new PubNub(credentials);
-      this.pubnub.subscribe({ channels: Object.values(CHANNELS) });
-      this.pubnub.addListener(this.listener());
-    }
-  
-    handleMessage(channel, message) {
-      console.log(`Message received. Channel: ${channel}. Message: ${message}`);
-  
-      const parsedMessage = JSON.parse(message);
-  
-      switch(channel) {
-        case CHANNELS.BLOCKCHAIN:
-          this.blockchain.replaceChain(parsedMessage, true, () => {
-            this.transactionPool.clearBlockchainTransactions({
-               chain: parsedMessage
-            });
+  TEST: "TEST",
+  BLOCKCHAIN: "BLOCKCHAIN",
+  TRANSACTION: "TRANSACTION",
+};
+
+class PubSub {
+  constructor({ blockchain, transactionPool, wallet }) {
+    this.blockchain = blockchain;
+    this.transactionPool = transactionPool;
+    this.wallet = wallet;
+
+    this.pubnub = new PubNub(credentials);
+    this.pubnub.subscribe({ channels: Object.values(CHANNELS) });
+    this.pubnub.addListener(this.listener());
+  }
+
+  handleMessage(channel, message) {
+    console.log(`Message received. Channel: ${channel}. Message: ${message}`);
+
+    const parsedMessage = JSON.parse(message);
+
+    switch (channel) {
+      case CHANNELS.BLOCKCHAIN:
+        this.blockchain.replaceChain(parsedMessage, true, () => {
+          this.transactionPool.clearBlockchainTransactions({
+            chain: parsedMessage,
           });
-          break;
-        case CHANNELS.TRANSACTION:
+        });
+        break;
+      case CHANNELS.TRANSACTION:
+        if (
+          !this.transactionPool.existingTransaction({
+            inputAddress: this.wallet.publicKey,
+          })
+        ) {
           this.transactionPool.setTransaction(parsedMessage);
-          break;
-        default:
-          return;
-      }
-    }
-  
-  
-    listener() {
-      return {
-        message: messageObject => {
-          const { channel, message } = messageObject;
-  
-          this.handleMessage(channel, message);
         }
-      };
-    }
-  
-    publish({ channel, message}) {
-      this.pubnub.publish({ channel, message });
-    }
-  
-    broadcastChain() {
-      this.publish({
-        channel: CHANNELS.BLOCKCHAIN,
-        message: JSON.stringify(this.blockchain.chain)
-      });
-    }
-  
-    broadcastTransaction(transaction) {
-      this.publish({
-        channel: CHANNELS.TRANSACTION,
-        message: JSON.stringify(transaction)
-      })
+        break;
+      default:
+        return;
     }
   }
-  
-  module.exports = PubSub;
+
+  listener() {
+    return {
+      message: (messageObject) => {
+        const { channel, message } = messageObject;
+
+        this.handleMessage(channel, message);
+      },
+    };
+  }
+
+  publish({ channel, message }) {
+    this.pubnub.publish({ channel, message });
+  }
+
+  broadcastChain() {
+    this.publish({
+      channel: CHANNELS.BLOCKCHAIN,
+      message: JSON.stringify(this.blockchain.chain),
+    });
+  }
+
+  broadcastTransaction(transaction) {
+    this.publish({
+      channel: CHANNELS.TRANSACTION,
+      message: JSON.stringify(transaction),
+    });
+  }
+}
+
+module.exports = PubSub;
--- index.js
+++ index.js
@@ -5,12 +5,19 @@ const Blockchain = require("./blockchain/index");
 const PubSub = require("./app/pubsub");
 const TransactionPool = require("./wallet/transaction-pool");
 const Wallet = require("./wallet");
+const TransactionMiner = require("./app/transaction-miner");
 
 const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
-const pubsub = new PubSub({ blockchain });
+const pubsub = new PubSub({ blockchain, transactionPool, wallet });
+const transactionMiner = new TransactionMiner({
+  blockchain,
+  transactionPool,
+  wallet,
+  pubsub,
+});
 
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
@@ -49,12 +56,22 @@ app.post("/api/transact", (req, res) => {
 
   transactionPool.setTransaction(transaction);
 
-  console.log("transactionPool", transactionPool);
+  pubsub.broadcastTransaction(transaction);
 
   res.json({ type: "success", transaction });
 });
 
-const syncChains = () => {
+app.get("/api/transaction-pool-map", (req, res) => {
+  res.json(transactionPool.transactionMap);
+});
+
+app.get("/api/mine-transactions", (req, res) => {
+  transactionMiner.mineTransactions();
+
+  res.redirect('api/blocks')
+});
+
+const syncWithRootState = () => {
   request(
     { url: `${ROOT_NODE_ADDRESS}/api/blocks` },
     (error, response, body) => {
@@ -63,6 +80,22 @@ const syncChains = () => {
 
         console.log("replace chain on a sync with", rootChain);
         blockchain.replaceChain(rootChain);
+      }
+    }
+  );
+
+  request(
+    { url: `${ROOT_NODE_ADDRESS}/api/transaction-pool-map` },
+    (error, resonse, body) => {
+      if (!error && response.statusCode == 200) {
+        const rootTransactionPoolMap = JSON.parse(body);
+
+        console.log(
+          "replace transaction pool map on a sync with",
+          rootTransactionPoolMap
+        );
+
+        transactionPool.setmap(rootTransactionPoolMap);
       }
     }
   );
@@ -79,6 +112,6 @@ app.listen(PORT, () => {
   console.log(`listening at localhost: ${PORT}`);
 
   if (PORT !== DEFAULT_PORT) {
-    syncChains();
+    syncWithRootState();
   }
 });
